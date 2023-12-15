c6377c19d36181b17a0d11644a9747d9567f54d7
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-12-15 23:43:22 +0530

    Mern skeleton

---
+++ Practice/MERN-crash/backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config('./.env');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5001;
+const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
+
+// Parses json data in request body
+app.use(express.json());
+// Parses URL Encoded data in request body
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
